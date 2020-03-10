9365bec1996610511a9316fbbe45a231119cd4ee
Kylie Hess <kyliemhess@gmail.com>
2020-03-10 00:52:08 -0500

    endless tweaking

--- client/components/AllBooksTable.js
+++ client/components/AllBooksTable.js
@@ -15,52 +15,57 @@ function AllBooksTable(props) {
 
   return (
     <div>
-      <h2>All Books</h2>
-      <button type="button">
+      <h2 className="title">All Books</h2>
+      <button type="button" className="button is-primary">
         <Link to="/addBook">Add Book</Link>
       </button>
-      <table className="table is-bordered is-striped is-narrow is-hoverable is-fullwidth">
-        <tbody>
-          <tr>
-            <th>ID</th>
-            <th>Title</th>
-            <th>Author</th>
-            <th>Genre</th>
-            <th>Price</th>
-            <th>Quantity</th>
-            <th>Edit</th>
-            <th>Delete</th>
-          </tr>
-          {props.books.length ? (
-            props.books.map(book => {
-              return (
-                <tr key={book.id}>
-                  <td>{book.id}</td>
-                  <td>{book.title}</td>
-                  <td>{book.author}</td>
-                  <td>{book.genre}</td>
-                  <td>{book.price}</td>
-                  <td>{book.quantity}</td>
-                  <td>
-                    <button type="button">
-                      <Link to={`/editBook/${book.id}`}>Edit</Link>
-                    </button>
-                  </td>
-                  <td>
-                    <button type="button" onClick={() => handleDelete(book.id)}>
-                      X
-                    </button>
-                  </td>
-                </tr>
-              )
-            })
-          ) : (
+      <div className="table-container">
+        <table className="table is-bordered is-striped is-narrow is-hoverable is-fullwidth">
+          <tbody>
             <tr>
-              <td>Loading...</td>
+              <th>ID</th>
+              <th>Title</th>
+              <th>Author</th>
+              <th>Genre</th>
+              <th>Price</th>
+              <th>Quantity</th>
+              <th>Edit</th>
+              <th>Delete</th>
             </tr>
-          )}
-        </tbody>
-      </table>
+            {props.books.length ? (
+              props.books.map(book => {
+                return (
+                  <tr key={book.id}>
+                    <td>{book.id}</td>
+                    <td>{book.title}</td>
+                    <td>{book.author}</td>
+                    <td>{book.genre}</td>
+                    <td>{book.price}</td>
+                    <td>{book.quantity}</td>
+                    <td>
+                      <button type="button">
+                        <Link to={`/editBook/${book.id}`}>Edit</Link>
+                      </button>
+                    </td>
+                    <td>
+                      <button
+                        type="button"
+                        onClick={() => handleDelete(book.id)}
+                      >
+                        X
+                      </button>
+                    </td>
+                  </tr>
+                )
+              })
+            ) : (
+              <tr>
+                <td>Loading...</td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
     </div>
   )
 }
--- client/components/AllUsers.js
+++ client/components/AllUsers.js
@@ -15,47 +15,52 @@ function AllUsers(props) {
 
   return (
     <div>
-      <h2>All Users</h2>
-      <table className="table is-bordered is-striped is-narrow is-hoverable is-fullwidth">
-        <tbody>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Address</th>
-            <th>Admin?</th>
-            <th>Edit</th>
-            <th>Delete</th>
-          </tr>
-          {props.users.length ? (
-            props.users.map(user => {
-              return (
-                <tr key={user.id}>
-                  <td>{user.id}</td>
-                  <td>{user.name}</td>
-                  <td>{user.email}</td>
-                  <td>{user.address}</td>
-                  {user.adminAccess ? <td>yes</td> : <td>no</td>}
-                  <td>
-                    <button type="button">
-                      <Link to="/user">Edit</Link>
-                    </button>
-                  </td>
-                  <td>
-                    <button type="button" onClick={() => handleDelete(user.id)}>
-                      X
-                    </button>
-                  </td>
-                </tr>
-              )
-            })
-          ) : (
+      <h2 className="title">All Users</h2>
+      <div className="table-container">
+        <table className="table is-bordered is-striped is-narrow is-hoverable is-fullwidth">
+          <tbody>
             <tr>
-              <td>Loading...</td>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Address</th>
+              <th>Admin?</th>
+              <th>Edit</th>
+              <th>Delete</th>
             </tr>
-          )}
-        </tbody>
-      </table>
+            {props.users.length ? (
+              props.users.map(user => {
+                return (
+                  <tr key={user.id}>
+                    <td>{user.id}</td>
+                    <td>{user.name}</td>
+                    <td>{user.email}</td>
+                    <td>{user.address}</td>
+                    {user.adminAccess ? <td>yes</td> : <td>no</td>}
+                    <td>
+                      <button type="button">
+                        <Link to="/user">Edit</Link>
+                      </button>
+                    </td>
+                    <td>
+                      <button
+                        type="button"
+                        onClick={() => handleDelete(user.id)}
+                      >
+                        X
+                      </button>
+                    </td>
+                  </tr>
+                )
+              })
+            ) : (
+              <tr>
+                <td>Loading...</td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
     </div>
   )
 }
