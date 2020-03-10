e71e7f8cda062777ebd7366c6bc3d84b33b1d040
Kylie Hess <kyliemhess@gmail.com>
2020-03-10 17:28:37 -0500

    deleted favicon

--- client/components/Checkout.js
+++ client/components/Checkout.js
@@ -6,12 +6,12 @@ function Checkout(props) {
   if (!props.cart) props.cart = []
 
   return (
-    <div className="columns">
-      <ol className="column is-half">
+    <div className="marginAll columns">
+      <ol className="column is-third">
         {props.cart.length ? (
           props.cart.map(item => {
             return (
-              <li className="orderItem card" key={item.id}>
+              <li className="orderItem box" key={item.id}>
                 <h3>Title: {item.book.title}</h3>
                 <h5>${item.book.price}</h5>
                 <p>quantity: {item.quantity}</p>
@@ -19,95 +19,103 @@ function Checkout(props) {
             )
           })
         ) : (
-          <p className="card">Your Cart Is Empty</p>
+          <p className="box">Your Cart Is Empty</p>
         )}
         <br />
-        <p className="card">Billing Information Placeholder</p>
       </ol>
-      <form className="column is-half card">
+
+      <form className="column is-third box">
         <div className="field">
-          <label htmlFor="firstname" className="label">
-            First Name:{' '}
-          </label>
           <div className="control">
+            <label htmlFor="firstname" className="label">
+              First Name:{' '}
+            </label>
             <input name="firstname" />
           </div>
         </div>
+
         <div className="field">
-          <label htmlFor="lastname" className="label">
-            Last Name:{' '}
-          </label>
           <div className="control">
+            <label htmlFor="lastname" className="label">
+              Last Name:{' '}
+            </label>
             <input name="lastname" />
           </div>
         </div>
+
         <div className="field">
-          <label htmlFor="address" className="label">
-            Address:{' '}
-          </label>
           <div className="control">
+            <label htmlFor="address" className="label">
+              Address:{' '}
+            </label>
             <input name="address" />
           </div>
         </div>
+
         <div className="field">
-          <label htmlFor="zipcode" className="label">
-            Zip:{' '}
-          </label>
           <div className="control">
+            <label htmlFor="zipcode" className="label">
+              Zip:{' '}
+            </label>
             <input name="zipcode" />
           </div>
         </div>
 
         <div className="field">
-          <label htmlFor="city" className="label">
-            City:{' '}
-          </label>
           <div className="control">
+            <label htmlFor="city" className="label">
+              City:{' '}
+            </label>
             <input name="city" />
           </div>
         </div>
 
         <div className="field">
-          <label htmlFor="state" className="label">
-            State:{' '}
-          </label>
           <div className="control">
+            <label htmlFor="state" className="label">
+              State:{' '}
+            </label>
             <input name="state" />
           </div>
         </div>
 
         <div className="field">
-          <label htmlFor="phone" className="label">
-            Phone Number:{' '}
-          </label>
           <div className="control">
+            <label htmlFor="phone" className="label">
+              Phone Number:{' '}
+            </label>
             <input name="phone" />
           </div>
         </div>
 
         <div className="field">
-          <label htmlFor="country" className="label">
-            Country:{' '}
-          </label>
           <div className="control">
+            <label htmlFor="country" className="label">
+              Country:{' '}
+            </label>
             <input name="country" />
           </div>
         </div>
 
         <div className="field">
-          <label htmlFor="child" className="label">
-            First Born Child:{' '}
-          </label>
           <div className="control">
+            <label htmlFor="child" className="label">
+              First Born Child:{' '}
+            </label>
             <input name="child" />
           </div>
         </div>
       </form>
-      <Link to="/confirmation">
-        <button type="button" className="button">
-          Next Step!
-        </button>
-      </Link>
+
+      <div className="column box is-third">
+        Billing Information Placeholder
+        <br />
+        <Link to="/confirmation">
+          <button type="button" className="button">
+            Next Step!
+          </button>
+        </Link>
+      </div>
     </div>
   )
 }
