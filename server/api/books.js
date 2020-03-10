2e62e6db69662c92aafcc5e62c76160723d8c879
Milos Djordjevic <milosdjor011@gmail.com>
2020-03-10 11:32:17 -0500

    search feature working

--- server/api/books.js
+++ server/api/books.js
@@ -1,31 +1,17 @@
 const router = require('express').Router()
 const {Book, Review, User} = require('../db/models')
+const Sequelize = require('sequelize')
+
 module.exports = router
 
-const limitBooks = [10, 20]
-
-router.get('/10/:pageId', async (req, res, next) => {
-  try {
-    const count = await Book.count()
-    console.log('gimme count', count)
-    const books = await Book.findAll({
-      attributes: ['title', 'imageUrl', 'price', 'author', 'id'],
-      limit: limitBooks[0],
-      offset: (req.params.pageId - 1) * limitBooks[0]
-    })
-    if (books) res.send([books, count])
-    else res.sendStatus(500)
-  } catch (err) {
-    next(err)
-  }
-})
-router.get('/20/:pageId', async (req, res, next) => {
+router.get('/:limit/:pageId/:search(*)', async (req, res, next) => {
   try {
     const count = await Book.count()
     const books = await Book.findAll({
       attributes: ['title', 'imageUrl', 'price', 'author', 'id'],
-      limit: limitBooks[1],
-      offset: (req.params.pageId - 1) * limitBooks[1]
+      limit: parseInt(req.params.limit),
+      where: {title: {[Sequelize.Op.iLike]: `%${req.params.search}%`}},
+      offset: (req.params.pageId - 1) * parseInt(req.params.limit)
     })
     if (books) res.send([books, count])
     else res.sendStatus(500)
@@ -61,6 +47,20 @@ router.post('/:id', async (req, res, next) => {
       })
       res.status(201).send(book)
     } else res.sendStatus(403)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.get('/search/:searchParam', async (req, res, next) => {
+  try {
+    const books = await Book.findAll({
+      where: {
+        search: req.params.title
+      }
+    })
+    console.log(req.params)
+    res.json(books)
   } catch (err) {
     next(err)
   }
