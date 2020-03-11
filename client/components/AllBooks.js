97a4213b6456beed64b69d6cd8da694085714ebd
Milos Djordjevic <milosdjor011@gmail.com>
2020-03-10 20:27:47 -0500

    styling done for all books

--- client/components/AllBooks.js
+++ client/components/AllBooks.js
@@ -18,68 +18,92 @@ function AllBooks(props) {
   )
 
   return (
-    <div className="columns is-multiline is-centered">
-      {props.books.length
-        ? props.books.map(book => {
-            return (
-              <div
-                className="column is-one-quarter has-text-centered "
-                key={book.id}
-              >
-                <div className="card animated marginTop">
-                  <Link to={`/books/${book.id}`} className="allBookSingle">
-                    <h3 className="titleAllBooks">{book.title}</h3>
-                    <img src={book.imageUrl} className="animated bounceInUp" />
-                    <p className="subtitleAllBooks">${book.price}</p>
-                  </Link>
+    <div className="upperPagination">
+      {paginationLimit.map(limited => (
+        <button
+          key={limited}
+          type="button"
+          className="pagination-button"
+          onClick={() => {
+            setLimit(limited)
+            setPage(1)
+          }}
+        >
+          {limited}
+        </button>
+      ))}
+      <div className="search" id="search-bar">
+        <form>
+          <input
+            className="searchTerm"
+            name="search"
+            onChange={evt => setSearch(evt.target.value)}
+            value={search}
+            placeholder="Search"
+          />
+        </form>
+      </div>
+
+      <div className="columns is-multiline is-centered">
+        {props.books.length
+          ? props.books.map(book => {
+              return (
+                <div
+                  className="column is-one-quarter has-text-centered "
+                  key={book.id}
+                >
+                  <div className="card animated marginTop">
+                    <Link to={`/books/${book.id}`} className="allBookSingle">
+                      <h3 className="titleAllBooks">{book.title}</h3>
+                      <img
+                        src={book.imageUrl}
+                        className="animated bounceInUp"
+                      />
+                      <p className="subtitleAllBooks">${book.price}</p>
+                    </Link>
+                  </div>
                 </div>
-              </div>
-            )
-          })
-        : false}
+              )
+            })
+          : false}
+      </div>
       <div>
-        <div className="menu" id="search-bar">
-          <form>
-            <input
-              name="search"
-              onChange={evt => setSearch(evt.target.value)}
-              value={search}
-              placeholder="Search"
-            />
-          </form>
-        </div>
-        <div>
-          {paginationLimit.map(limited => (
-            <button
-              key={limited}
-              type="button"
-              onClick={() => {
-                setLimit(limited)
-                setPage(1)
-              }}
-            >
-              {limited}
-            </button>
-          ))}
-          <button type="button" onClick={() => setPage(1)}>
+        <div className="pages-books">
+          <button
+            type="button"
+            className="allbooks-button"
+            onClick={() => setPage(1)}
+          >
             First Page
           </button>
           {page > 1 ? (
-            <button type="button" onClick={() => setPage(page - 1)}>
+            <button
+              type="button"
+              className="allbooks-button"
+              onClick={() => setPage(page - 1)}
+            >
               Previous Page
             </button>
           ) : (
             false
           )}
           {page < number ? (
-            <button type="button" onClick={() => setPage(page + 1)}>
+            <button
+              type="button"
+              className="allbooks-button"
+              onClick={() => setPage(page + 1)}
+            >
               Next Page
             </button>
           ) : (
             false
           )}
           {page !== number ? (
-            <button type="button" onClick={() => setPage(props.pageNumber)}>
+            <button
+              type="button"
+              className="allbooks-button"
+              onClick={() => setPage(props.pageNumber)}
+            >
               Last Page
             </button>
           ) : (
