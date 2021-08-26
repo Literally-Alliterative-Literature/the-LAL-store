066fbab67fd061d402c272972d098774429467bf
Kylie Hess <kyliemhess@gmail.com>
2021-08-25 21:25:47 -0500

    dialect options

--- server/db/db.js
+++ server/db/db.js
@@ -6,9 +6,12 @@ const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
 const db = new Sequelize(
   process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
   {
-    ssl: {
-      rejectUnauthorized: false
-    },
+    dialectOptions: {
+      ssl: {
+          require: true,
+          rejectUnauthorized: false
+      }
+  },
     logging: false
   }
 )
